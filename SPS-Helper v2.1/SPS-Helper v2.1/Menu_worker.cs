266e419228240e24fb4ca78687e8f0db7501e53c
Сафаргалиев Тимур <tsafargaliev@maxus.ru>
2018-04-10 11:59:39 +0300

    Незначительные доработки
    Добавдены:
    * обработчик файла меню Menu worker
    * класс "крипичик" команды для графического построения меню
    * картинка для блока PowerShell

---
+++ SPS-Helper v2.1/SPS-Helper v2.1/Menu_worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SPS_Helper
+{
+    class Menu_worker
+    {
+        public static void Load()
+        {
+            XmlDocument menu_file = new XmlDocument();
+            return;
+        }
+    }
+}
